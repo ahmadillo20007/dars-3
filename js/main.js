115a99485fd90ea0dbe3f7f174c0500f5c40132e
Ahmadillo20007 <ahmadilloqubijonov@gmail.com>
2022-12-09 20:09:21 +0500

    a

---
+++ js/main.js
@@ -0,0 +1,19 @@
+alert('Hush kelibsiz')
+    alert('Galareyaga ruxsat berasizmi ')
+    alert('locationga ruxsat berasizmi ')
+    var a = prompt('Ismingizni kiriting')
+    var b = prompt('Telefon raqamingiz ')
+    var c = prompt('Yoshingiz ')
+
+
+
+
+
+
+
+
+
+
+console.log(a);
+console.log(b);
+console.log(c);
